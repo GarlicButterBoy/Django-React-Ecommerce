eced0c72bba71382440169c1fd8532f8983c91ce
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2022-09-21 10:44:56 -0400

    Create db structure and models

---
+++ .history/backend/base/models_20220921100900.py
@@ -0,0 +1,14 @@
+from unicodedata import name
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Product(models.Model):
+    user =
+    name = 
+    image = 
+    brand = 
+    category = 
+    description = 
+    rating = 
